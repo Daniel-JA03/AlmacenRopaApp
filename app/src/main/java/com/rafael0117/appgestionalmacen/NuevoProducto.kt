d1fd8b57bc1f81214c50244b7ceddd4f8196de30
rafael0117 <mamanirafael324@gmail.com>
2025-06-21 17:00:02 -0500

    Agregue Activity Producto y NuevoProducto

---
+++ app/src/main/java/com/rafael0117/appgestionalmacen/NuevoProducto.kt
@@ -0,0 +1,20 @@
+package com.rafael0117.appgestionalmacen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class NuevoProducto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_nuevo_producto)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
